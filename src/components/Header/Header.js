945d82144ba1b4207441634b7b8a3a7c5a154ac7
thakurSaad <thakursaad613@gmail.com>
2022-04-04 14:14:52 +0600

    navbar style done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="bg-blue-900 text-white py-3">
-      <Link className="mr-4" to="/">Home</Link>
-      <Link className="mr-4" to="/reviews">Reviews</Link>
-      <Link className="mr-4" to="/dashboard">Dashboard</Link>
-      <Link className="mr-4" to="/blogs">Blogs</Link>
-      <Link className="mr-4" to="/about">About</Link>
+      <Link className="mr-10 hover:text-gray-400 duration-150" to="/">Home</Link>
+      <Link className="mr-10 hover:text-gray-400 duration-150" to="/reviews">Reviews</Link>
+      <Link className="mr-10 hover:text-gray-400 duration-150" to="/dashboard">Dashboard</Link>
+      <Link className="mr-10 hover:text-gray-400 duration-150" to="/blogs">Blogs</Link>
+      <Link className="mr-10 hover:text-gray-400 duration-150" to="/about">About</Link>
     </div>
   );
 };
