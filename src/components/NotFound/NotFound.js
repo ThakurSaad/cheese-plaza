41c8f288efe717c73c24cf2e594a375c13c0fc77
thakurSaad <thakursaad613@gmail.com>
2022-04-04 17:22:58 +0600

    notFount route, review margin

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="text-center mt-20">
+      <p className="text-3xl font-semibold">
+        Page Not Found ! <br /> 404
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -7,7 +7,7 @@ const Reviews = () => {
   const [reviews, setReviews] = useReviews();
 
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 box-border">
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 box-border mt-4 mb-20">
       {reviews.map((review) => (
         <Review key={review.id} review={review}></Review>
       ))}
